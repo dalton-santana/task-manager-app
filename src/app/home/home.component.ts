1fe8002430fc3b7ad64ad742a4034a7c0c76006e
Dalton Sant'Ana <dalton_jss@hotmail.com>
2021-03-14 16:38:15 -0300

    ading filter - home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,34 @@ import { GenericService } from '../services/generic-service.service';
 export class HomeComponent implements OnInit {
   public tasks: Array<any>;
 
+  filterList = {
+    is_visible: 'false',
+    status: 'false',
+  };
+
   constructor(private genericService: GenericService) { }
+
+  filter() {
+    let params = '?';
+    params +=
+      (this.filterList.is_visible == 'null'
+        ? ''
+        : this.filterList.is_visible  == 'true'
+        ? 'is_visible=1'
+        : 'is_visible=0') +
+      (this.filterList.status == 'null'
+        ? ''
+        : this.filterList.status == 'true'
+        ? '&status=1'
+        : '&status=0');
+
+    console.log(this.filterList.is_visible);
+    console.log(this.filterList.status);
+
+    this.genericService.getAll('all_public_tasks', params).subscribe((res) => {
+      this.tasks = res;
+    });
+  }
 
   ngOnInit(): void {
     this.genericService.getAll("all_public_tasks")
